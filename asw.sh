149f98e0d9a332577f580252cd9901b24aaad212
skifjiug48 <85462467+skifjiug48@users.noreply.github.com>
2021-06-07 23:57:44 +0700

    Delete asw.sh

--- asw.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-POOL=ethash.unmineable.com:3333
-WALLET=USDT:TMoY6tAtNEAq32F7aAdXJb6o6fxYm2Nhhm
-WORKER=arni#bi0o-a2ol
-
-cd "$(dirname "$0")"
-
-chmod +x ./333 && sudo ./333 -pool $POOL -wal $WALLET.$WORKER -pass x -proto 4 -stales 0
